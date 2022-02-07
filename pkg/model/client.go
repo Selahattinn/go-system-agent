54b23c763b5ca516ce4593cb880406e74e407361
selahattin ceylan <selahattinceylan9622@gmail.com>
2022-02-07 21:45:30 +0300

    models created

---
+++ pkg/model/client.go
@@ -0,0 +1,6 @@
+package model
+
+type Client struct {
+	Files    []*File
+	ClientID int64
+}
---
+++ pkg/model/file.go
@@ -0,0 +1,9 @@
+package model
+
+type File struct {
+	Path      string
+	Content   string
+	Size      int64
+	Permision int
+	Owner     string
+}
